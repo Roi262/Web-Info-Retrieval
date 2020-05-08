bd80cbd45c5cf87233a9f88af8458be377826f11
Roi262 <roi262@gmail.com>
2020-05-08 23:10:25 +0300

    finished featuresdict. need to debug

--- src/webdata/FeaturesDict.java
+++ src/webdata/FeaturesDict.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FeaturesDict {
-    private static final int PRODUCT_ID = 0;
-    private static final int PRODUCT_ID = 0;
-    private static final int PRODUCT_ID = 0;
-    private static final int PRODUCT_ID = 0;
-    private static final int PRODUCT_ID = 0;
+    private static final int SCORE = 0;
+    private static final int HELP_N = 1;
+    private static final int HELP_D = 2;
+    private static final int REVIEW_LEN = 3;
+    private static final String SEPARATOR = "#";
 
     private ArrayList<String> dict;
 
@@ -29,15 +29,29 @@ public class FeaturesDict {
      */
     public void add(String productID, Integer score,
                     Integer helpNumerator, Integer helpDenominator, Integer reviewLen) {
-        String values = productID + DeltaCompressor.compressList(
+        String values = productID + SEPARATOR + DeltaCompressor.compressList(
                 new ArrayList<>(Arrays.asList(score, helpNumerator, helpDenominator, reviewLen)));
         dict.add(values);
     }
 
     public String getProductID(int reviewID){
-        ArrayList<Integer> values = DeltaCompressor.decode(dict.get(reviewID));
-        return values.get(PRODUCT_ID);
+        String value = dict.get(reviewID);
+        return splitOnSeparator(value)[0];
+    }
+
+    public int getValue(int reviewID, int type){
+        assert type == SCORE || type == HELP_D || type == HELP_N || type == REVIEW_LEN;
+
+        String value = dict.get(reviewID);
+        String binaryString = splitOnSeparator(value)[1];
+        ArrayList<Integer> values = DeltaCompressor.deltaDecode(binaryString);
+        return values.get(type);
     }
 
 
+    private static String[] splitOnSeparator(String str){
+        String[] list = str.split(SEPARATOR);
+        assert list.length == 2;
+        return list;
+    }
 }
